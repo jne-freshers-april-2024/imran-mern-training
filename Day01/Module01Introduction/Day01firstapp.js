c38e5ece46de9939e5db3d702a5f50f6dec0ff34
ImranBagwan1999 <imran.bagwan@thinkitive.com>
2024-04-09 20:01:40 +0530

    Nodejs Day01 Training

---
+++ Day01/Module01Introduction/Day01firstapp.js
@@ -0,0 +1,7 @@
+const fs = require('fs');
+
+// fs is inbuild funcionality to access and work with file system
+
+fs.writeFileSync('hello.txt','hello from nodeJs');
+
+console.log('Hello from nodeJs');
