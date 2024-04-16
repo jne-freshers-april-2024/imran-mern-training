dee42c20ab6da2ee551a64e43400e26eb236f5f3
ImranBagwan1999 <imran.bagwan@thinkitive.com>
2024-04-16 10:08:38 +0530

    Day06 AuthenticationSessionEmails

---
+++ Day06/Module15Authentication/controllers/employeeController.js
@@ -0,0 +1,135 @@
+const Employee = require('../models/employeeModel');
+const bcrypt = require('bcryptjs');
+
+
+const employeeRegisterPage =(req,res,next)=>{
+    // console.log(req.get('Cookie').split('=')[1]);
+     res.send(
+    `<form class='' action='/employee/addEmployee' method='post'>
+     <div class=''>
+      <label for='name'>userName</label>
+      <input type='text'name='name'id='name'></input>
+     </div>
+     <div class=''>
+     <label for='company'> Company </label>
+      <input type='text' name='company' id='company'></input>
+     </div>
+     <div class=''>
+     <label for='email'> E-mail </label>
+      <input type='email' name='email' id='email'></input>
+     </div>
+     <div class=''>
+     <label for='password'> Password </label>
+      <input type='password' name='password' id='password'></input>
+     </div>
+     <button class='btn' type='submit'>Login</button>
+  </form>`)
+
+}
+
+
+const employeeloginPage =(req,res,next)=>{
+    // console.log(req.get('Cookie').split('=')[1]);
+     res.send(
+    `<form class='' action='/employee/validateEmployee' method='post'>
+     <div class=''>
+     <label for='email'> E-mail </label>
+      <input type='email' name='email' id='email'></input>
+     </div>
+     <div class=''>
+     <label for='password'> Password </label>
+      <input type='password' name='password' id='password'></input>
+     </div>
+     <button class='btn' type='submit'>Login</button>
+  </form>`)
+
+}
+
+const addEmployee = (req,res,next)=>{
+    const u = req.body;
+    console.log(u);
+
+    Employee.findOne({email:u.email})
+    .then((employeeData)=>{
+        //console.log("employeeData ",employeeData)
+        if(employeeData){
+            res.redirect('/employee/register');
+        }else{
+            // 12 denotes no of level it can be hashed return promise
+         return   bcrypt.hash(u.password,12)
+         .then((hashPassword)=>{
+            const employee = new Employee({
+                name : u.name,
+                company:u.company,
+                email:u.email,
+                password:hashPassword
+            });
+        
+            employee.save()
+            .then((resolve)=>{
+                res.redirect('/employee/login');
+            }).catch((reject)=>{
+                   res.json({
+                     "message":"User is not added..."
+                   })
+            })
+        }); 
+          
+        }
+    }).catch((err)=>{
+          res.send('error in finding employee')
+    })
+   
+}
+
+const validateEmployee = (req,res,next)=>{
+     const employee = req.body;
+     console.log(employee);
+     Employee.findOne({email:employee.email})
+     .then((e)=>{
+              if(!e){
+                console.log("e",e);
+                return res.redirect('/employee/login')
+              }
+              bcrypt.compare(employee.password,e.password).then((match)=>{
+                     if(match){
+                        console.log("password mathches....")
+                         req.session.isLoggedIn = true;
+                         req.session.employee = e;
+                         return req.session.save(err=>{
+                            console.log(err);
+                            res.send('employee login successfully');
+                         })
+                     }
+                         res.redirect('/employee/login');
+                     
+              }).catch((err)=>{
+                console.log("inside catch 9....");
+                return res.redirect('/employee/login');
+              })
+     }).catch((error)=>{
+        console.log('inside catch...10');
+         res.redirect('/employee/login');
+     })
+}
+
+const allData = (req,res,next)=>{
+    //  if(req.session.isLoggedIn)
+    //     res.send('Protected routes');
+    //  else{
+    //     console.log('used is not logged in');
+    //     res.redirect('/employee/login')
+    //  }
+
+        res.send('Protected routes');
+       
+    }
+
+module.exports ={
+     employeeRegisterPage,
+     addEmployee,
+     employeeloginPage,
+     validateEmployee,
+     allData
+
+}
---
+++ Day06/Module16SendingEmails/01SessionsAndCookies.js
@@ -0,0 +1,23 @@
+
+// to persist data in session and cookies to handle user requests 
+// for same session
+
+// we store user data in memory and client side to handle same user request
+// if user logged in we need to sored information that user for next same user request to handle that
+// Basic that we need member user all his requst that session
+// that why we use session or cookies
+
+
+// every request treated as a separte 
+// we need to set everyTime
+
+// also we have alternative of global variable -- but this can shared we multipal users
+
+
+// cookie-parse we can use that module
+
+// http request every client-server request forget about each other
+// we need to remender our client that we need to use session
+// session stored on server
+// cookies are stroed on client browser
+
