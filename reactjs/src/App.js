3a6f797a0d9d3c7d4bee0f70b89179e51ee6b995
ImranBagwan1999 <imran.bagwan@thinkitive.com>
2024-04-23 11:49:17 +0530

    Functional Components used useState props functionality

--- reactjs/src/App.js
+++ reactjs/src/App.js
@@ -1,13 +1,18 @@
 import React from "react";
 import HeaderComponent from "./components/HeaderComponents";
 import ConceptComponent from "./components/ConceptComponent";
+import FunctionalComponents from './components/DataComponent/FunctionalCompontents'
 import "./App.css";
 
 const App = () => {
   return (
+    // <div>
+    //   <HeaderComponent />
+    //   <ConceptComponent />
+    // </div>
+
     <div>
-      <HeaderComponent />
-      <ConceptComponent />
+      <FunctionalComponents></FunctionalComponents>
     </div>
   );
 };
