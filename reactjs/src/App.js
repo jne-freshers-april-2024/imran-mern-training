48743be5b25260075b88fd543319f1674ad88599
ImranBagwan1999 <imran.bagwan@thinkitive.com>
2024-04-23 11:51:43 +0530

    Employee From created and Display list of employee

--- reactjs/src/App.js
+++ reactjs/src/App.js
@@ -2,6 +2,7 @@ import React from "react";
 import HeaderComponent from "./components/HeaderComponents";
 import ConceptComponent from "./components/ConceptComponent";
 import FunctionalComponents from './components/DataComponent/FunctionalCompontents'
+import EmployeeFormComponet from './components/EmployeeFormComponent'
 import "./App.css";
 
 const App = () => {
@@ -11,8 +12,12 @@ const App = () => {
     //   <ConceptComponent />
     // </div>
 
+    // <div>
+    //   <FunctionalComponents></FunctionalComponents>
+    // </div>
+
     <div>
-      <FunctionalComponents></FunctionalComponents>
+       <EmployeeFormComponet></EmployeeFormComponet>
     </div>
   );
 };
