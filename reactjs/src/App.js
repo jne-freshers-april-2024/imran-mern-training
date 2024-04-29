73e102155d331ada9daaccec220c5f7b2271b5e1
ImranBagwan1999 <imran.bagwan@thinkitive.com>
2024-04-29 20:45:49 +0530

    Only text file added

--- reactjs/src/App.js
+++ reactjs/src/App.js
@@ -3,17 +3,10 @@ import { Counter } from "./components/Counter";
 import Home from "./components/Home";
 import Aboutus from "./components/Aboutus";
 import Contact from "./components/Contact";
-import {BrowserRouter, Route, Routes} from 'react-router-dom'
 const App = () => {
   return (
     <React.Fragment>
-      <BrowserRouter>
-           <Routes>
-           <Route path="about" element={<Aboutus />} />
-          <Route path="contact" element={<Contact />} />
-          <Route path="/" element={<Home />} />
-           </Routes>
-      </BrowserRouter>
+      
     </React.Fragment>
     // <div>
     //   <Counter />
