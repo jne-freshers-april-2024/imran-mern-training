674d59e2dc5a0556beb293917c2f0f67f2201326
ImranBagwan1999 <imran.bagwan@thinkitive.com>
2024-04-29 20:23:55 +0530

    Browser-Router

--- reactjs/src/App.js
+++ reactjs/src/App.js
@@ -1,11 +1,23 @@
 import React from "react";
 import { Counter } from "./components/Counter";
-
+import Home from "./components/Home";
+import Aboutus from "./components/Aboutus";
+import Contact from "./components/Contact";
+import {BrowserRouter, Route, Routes} from 'react-router-dom'
 const App = () => {
   return (
-    <div>
-      <Counter />
-    </div>
+    <React.Fragment>
+      <BrowserRouter>
+           <Routes>
+           <Route path="about" element={<Aboutus />} />
+          <Route path="contact" element={<Contact />} />
+          <Route path="/" element={<Home />} />
+           </Routes>
+      </BrowserRouter>
+    </React.Fragment>
+    // <div>
+    //   <Counter />
+    // </div>
   );
 };
 
